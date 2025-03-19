cb7e8e12d2d816443c6ad9d3fa760804fc243336
SebbyLam <sebastian.lam1@education.nsw.gov.au>
2025-03-20 09:40:24 +1100

    eee
    ee

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -1,24 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+
 public class EnemyMovement : MonoBehaviour
 {
+ // Reference to the player's transform.
+ public Transform player;
 
-    public Transform player;
-    private NavMeshAgent navMeshAgent;
-    // Start is called before the first frame update
-    void Start()
+ // Reference to the NavMeshAgent component for pathfinding.
+ private NavMeshAgent navMeshAgent;
+
+ // Start is called before the first frame update.
+ void Start()
     {
+ // Get and store the NavMeshAgent component attached to this object.
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+ // Update is called once per frame.
+ void Update()
     {
-               if (player != null)
-       {    
-           navMeshAgent.SetDestination(player.position);
-       }
+ // If there's a reference to the player...
+ if (player != null)
+        {    
+ // Set the enemy's destination to the player's current position.
+            navMeshAgent.SetDestination(player.position);
+        }
     }
 }
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,82 +1,113 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-  
+ // Rigidbody of the player.
  private Rigidbody rb; 
 
- 
+ // Variable to keep track of collected "PickUp" objects.
  private int count;
 
- 
+ // Movement along X and Y axes.
  private float movementX;
  private float movementY;
 
- 
+ // Speed at which the player moves.
  public float speed = 0;
 
- 
- public TextMeshProUGUI CountText;
+ // UI text component to display count of "PickUp" objects collected.
+ public TextMeshProUGUI countText;
 
-
+ // UI object to display winning text.
  public GameObject WinTextObject;
-    void Start()
+
+ // Start is called before the first frame update.
+ void Start()
     {
+ // Get and store the Rigidbody component attached to the player.
         rb = GetComponent<Rigidbody>();
-        WinTextObject.SetActive(false);
+
+ // Initialize count to zero.
+        count = 0;
+
+ // Update the count display.
         SetCountText();
-        count = 1;
+
+ // Initially set the win text to be inactive.
+        WinTextObject.SetActive(false);
+    }
  
+ // This function is called when a move input is detected.
+ void OnMove(InputValue movementValue)
+    {
+ // Convert the input value into a Vector2 for movement.
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+ // Store the X and Y components of the movement.
+        movementX = movementVector.x; 
+        movementY = movementVector.y; 
     }
 
-    // Update is called once per frame
- void OnMove (InputValue movementValue)
-   {
-   Vector2 movementVector = movementValue.Get<Vector2>();
+ // FixedUpdate is called once per fixed frame-rate frame.
+ private void FixedUpdate() 
+    {
+ // Create a 3D movement vector using the X and Y inputs.
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
 
-    movementX = movementVector.x; 
-    movementY = movementVector.y;  
-   }
-      void SetCountText() 
-   {
-       CountText.text =  "Count: " + count.ToString();
-   }
-private void FixedUpdate()
-{
-    Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
-
-    rb.AddForce(movement * speed);
-} 
-
-private void OnTriggerEnter(Collider other)
-{
-    SetCountText();
-    if (other.gameObject.CompareTag("PickUp")) {
-        other.gameObject.SetActive(false);
-        count = count + 1;
-        if (count >= 12)
-       {
-           WinTextObject.SetActive(true);
-       }
-        
+ // Apply force to the Rigidbody to move the player.
+        rb.AddForce(movement * speed); 
     }
-    
-    
-}
+
+ 
+ void OnTriggerEnter(Collider other) 
+    {
+ // Check if the object the player collided with has the "PickUp" tag.
+ if (other.gameObject.CompareTag("PickUp")) 
+        {
+ // Deactivate the collided object (making it disappear).
+            other.gameObject.SetActive(false);
+
+ // Increment the count of "PickUp" objects collected.
+            count = count + 1;
+
+ // Update the count display.
+            SetCountText();
+        }
+    }
+
+ // Function to update the displayed count of "PickUp" objects collected.
+ void SetCountText() 
+    {
+ // Update the count text with the current count.
+        countText.text = "Count: " + count.ToString();
+
+ // Check if the count has reached or exceeded the win condition.
+ if (count >= 12)
+        {
+ // Display the win text.
+            WinTextObject.SetActive(true);
+
+ // Destroy the enemy GameObject.
+           Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+        }
+    }
+
 private void OnCollisionEnter(Collision collision)
 {
-   if (collision.gameObject.CompareTag("Enemy"))
-   {
-       // Destroy the current object
-       Destroy(gameObject); 
-       // Update the winText to display "You Lose!"
-       WinTextObject.gameObject.SetActive(true);
-       WinTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
-   }
+ if (collision.gameObject.CompareTag("Enemy"))
+    {
+ // Destroy the current object
+        Destroy(player.gameObject); 
+ 
+ // Update the winText to display "You Lose!"
+        WinTextObject.gameObject.SetActive(true);
+        WinTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+ 
+    }
+
 }
+
+
 }
